3831030e8d4997c224586e4c6dc235fa9f3d27eb
Disinor <disin2015@mail.ru>
2018-10-19 20:11:07 +0300

    Task 01

---
+++ src/ex4_js-objects-part2/task01.js
@@ -0,0 +1,25 @@
+var Calculator =function (a){
+    var result;
+    result=0;
+    return console.log(result)
+     if (typeof a==='number'){
+        result=0;
+
+        function getResult(){
+            result=0;
+        return result;
+    };
+
+    function add(a){
+        result+=a;
+        return result;
+    };
+}
+else console.log('Введено не число')
+}
+var a=Calculator.getResult
+console.log(a)
+//a=Calculator.add(2)
+console.log(a)
+
+
