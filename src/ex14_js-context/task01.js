cc059c9ee74f8c12d20d5581b00d30352ad87644
Disinor <disin2015@mail.ru>
2018-12-06 20:34:59 +0300

    Task01 Update

--- src/ex14_js-context/task01.js
+++ src/ex14_js-context/task01.js
@@ -1,59 +1,64 @@
-function Calculator(){
-   
-    return {
-          getResult: function(){
-                return this.result;
-      },
-      
-       add: function(a){
-        if (typeof a==='number'&&!isNaN(a))
-        this.result+=a;
-         return this;
-      },
-       subtract: function(a){
-        if (typeof a==='number'&&!isNaN(a))
-        this.result-=a;
-         return this;
-      },
-       divide: function(a){
-        if (typeof a==='number'&&!isNaN(a))
-        this.result/=a;
-         return this;
-      },
-      multiply: function(a){
-        if (typeof a==='number'&&!isNaN(a))
-        this.result*=a;
-        return this;
-      },
-       reset: function(){
-        this.result=0;
-        return this;
-      },
-      setState: function(a){
-        this.result=a;
-        return this;
-      }
-  }
-  }
-  
-  
-   var g = new  Calculator();
-  console.log(g.getResult() );
-  g.result =100;
-  console.log(g.getResult() );
-  g.add(2).reset();
-  console.log(g.getResult() );
-  g.add(1).multiply(2).divide(4).setState(3);
-  console.log(g.getResult() );
-  /*g.multiply()(2);
-  console.log(g.getResult() );
-  g.divide(3)(2);
-  console.log(g.getResult() );*/
-  const result =  g.add(100)
-  .multiply(2)
-  .divide(20)
-  .reset()
-  .subtract(1)
-  .getResult();
+function Calculator() {
+  var result = 0;
 
-console.log(result); // -1
+  return {
+
+    getResult: function () {
+
+      return result;
+    },
+
+    add: function (a) {
+      if (typeof a === 'number' && !isNaN(a))
+        result += a;
+      return this;
+    },
+    subtract: function (a) {
+      if (typeof a === 'number' && !isNaN(a))
+        result -= a;
+      return this;
+    },
+    divide: function (a) {
+      if (typeof a === 'number' && !isNaN(a))
+        result /= a;
+      return this;
+    },
+    multiply: function (a) {
+      if (typeof a === 'number' && !isNaN(a))
+        result *= a;
+      return this;
+    },
+    reset: function () {
+      result = 0;
+      return this;
+    },
+    setState: function (a) {
+      result = a;
+      return this;
+    }
+  }
+}
+
+
+var g = new Calculator();
+g.add(10);
+console.log(g.getResult());
+g.result = 100;
+console.log(g.getResult());
+// console.log(g.getResult() );
+// g.add(2).reset();
+// console.log(g.getResult() );
+// g.add(1).multiply(2).divide(4).setState(3);
+// console.log(g.getResult() );
+/*g.multiply()(2);
+console.log(g.getResult() );
+g.divide(3)(2);
+console.log(g.getResult() );*/
+// g.add(100)
+// .multiply(2)
+// .divide(20)
+// .reset()
+// .subtract(1)
+// .getResult();
+
+console.log(g.result); 
